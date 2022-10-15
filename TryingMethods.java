6f736f648f02c6eeed3d35ce72489c97e77ddca0
John Isutsah <johnisutsa@gmail.com>
2022-10-15 23:55:13 +0000

    Day function implementation

--- TryingMethods.java
+++ TryingMethods.java
@@ -3,5 +3,33 @@ public class TryingMethods {
         
     }
 
-    
+    String dayOfTheWeek(int day){
+        String dayOfTheWeek = "";
+        switch (day) {
+            case 1:
+                dayOfTheWeek = "Sun";
+                break;
+            case 2:
+                dayOfTheWeek = "Mon";
+                break;
+            case 3:
+                dayOfTheWeek = "Tue";
+                break;
+            case 4:
+                dayOfTheWeek = "Wed";
+                break;
+            case 5:
+                dayOfTheWeek = "Thu";
+                break;
+            case 6:
+                dayOfTheWeek = "Fri";
+                break;
+            case 7:
+                dayOfTheWeek = "Sat";
+                break;
+            default:
+                break;
+        }
+        return dayOfTheWeek;
+    }
 }
